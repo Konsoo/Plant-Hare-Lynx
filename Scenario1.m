ed8c0e4aeb47e3a6a44033ee9c627992508d9774
Konsoo <133098361+Konsoo@users.noreply.github.com>
2023-05-10 15:19:36 +0200

    Update Scenario1.m

--- Scenario1.m
+++ Scenario1.m
@@ -4,10 +4,10 @@ a1=3; % rate hares consumes plants
 a2=0.8; %rate which lynx cosume hares
 b1=0.3; %ow quickly the hare population growth is affected by the plant population
 b2=5; %how quickly the lynx population growth is affected by the hare population)
-d1=0.1; %deathrate hares natural
-d2=0.065; %lynxdeathrate natural
+d1=0.1; %natural detharte hares natural
+d2=0.065; %lnatural deathrate lynx
 t0 = 0;
-tfinal=100;
+tfinal=200;
 tspan=[t0 tfinal];
 y0=[.7 .35 0.5];
 
